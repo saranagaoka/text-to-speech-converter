3d6f0d9d2679c3da4717e53c250c0907d829224c
Sara <sara.nagaoka@gmail.com>
2023-03-23 23:32:33 +0100

    filter out eloquence languages

--- src/App.js
+++ src/App.js
@@ -5,15 +5,41 @@ import "./App.css";
 function App() {
   const [text, setText] = useState("Click here to edit the text");
   const [voices, setVoices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function iOS() {
+    return (
+      [
+        "iPad Simulator",
+        "iPhone Simulator",
+        "iPod Simulator",
+        "iPad",
+        "iPhone",
+        "iPod",
+      ].includes(navigator.platform) ||
+      // iPad on iOS 13 detection
+      (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+    );
+  }
 
   useEffect(() => {
     EasySpeech.detect();
     EasySpeech.init()
       .then(() => {
-        console.log(EasySpeech.status().voices);
-        setVoices(
-          EasySpeech.status().voices.filter((v) => v.lang.includes("en-"))
-        );
+        setIsLoading(false);
+        const voicesList = EasySpeech.status().voices;
+        if (iOS()) {
+          setVoices(
+            voicesList.filter(
+              (v) =>
+                v.lang.includes("en-") &&
+                !v.voiceURI.includes("com.apple.eloquence.") &&
+                (v.name.includes("Karen") || v.name.includes("Samantha"))
+            )
+          );
+        } else {
+          setVoices(voicesList.filter((v) => v.lang.includes("en-")));
+        }
       })
       .catch((e) => console.error("no speech synthesis:", e.message));
   }, []);
@@ -67,28 +93,33 @@ function App() {
           }}
         />
         <br />
-
-        {voices.map((voice) => (
-          <button
-            className="buttonStyle"
-            onClick={() => {
-              handleOnClick(voice);
-            }}
-            style={{
-              border: "none",
-              background: "rgb(55 129 30)",
-              color: "#fff",
-              padding: "8px 16px",
-              borderRadius: "30px",
-              fontWeight: 800,
-              boxShadow: "0 4px 10px #00000033",
-              cursor: "pointer",
-              marginBottom: "16px",
-            }}
-          >
-            Listen {voice.name}
-          </button>
-        ))}
+        {isLoading ? (
+          <p>Loading voices ...</p>
+        ) : voices.length === 0 ? (
+          <p>no voices detected</p>
+        ) : (
+          voices.map((voice) => (
+            <button
+              className="buttonStyle"
+              onClick={() => {
+                handleOnClick(voice);
+              }}
+              style={{
+                border: "none",
+                background: "rgb(55 129 30)",
+                color: "#fff",
+                padding: "8px 16px",
+                borderRadius: "30px",
+                fontWeight: 800,
+                boxShadow: "0 4px 10px #00000033",
+                cursor: "pointer",
+                marginTop: "16px",
+              }}
+            >
+              Listen {voice.name}
+            </button>
+          ))
+        )}
       </div>
     </div>
   );
